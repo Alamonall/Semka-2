66640364d408eb006abd2f45930706209220ebcf
Aleksey <aragorn_108@mail.ru>
2019-10-17 17:00:33 +0800

    0.1
    - Первыый коммит

--- config/inst.js
+++ config/inst.js
@@ -5,6 +5,7 @@ let xml2js = require('xml2js');
 let iconvlite = require('iconv-lite');
 let xmlBuilder = require('xmlbuilder');
 let parser = new xml2js.Parser({ attrkey: "ATTR" });
+const uuidv1 = require('uuid/v1');
 
 //получение папок проектов для резки изображений
 let inst = {
@@ -38,6 +39,7 @@ let inst = {
               console.log('exte: ' + path.extname(file));  
               console.log('filename: ' + path.parse(file).name);  
               console.log('dirname: ' + path.join(dir, item, file));
+              console.log('dir: ' + path.join(__dirname, 'projects'));
               aud_list.push(path.join(dir, item, path.parse(file).name));
           }
         }    
@@ -48,33 +50,56 @@ let inst = {
   cutting: function(list){
     let i = 0,arr = [], done;
     for(let item of list){
-      console.log('xml path: '+ item + '.XML');           
+      console.log('xml path: '+ item + '.XML');   
+      //console.log('outfile: ' + path.join(__dirname, 'projects/images/') + path.parse(item).name + '_' + i + '.png');        
       let xml_string = readFileSync_encoding(item + '.XML', 'UTF-16');//.replace(/\?\?/,'');
       parser.parseString(xml_string,
       (err, res)=>{
-        done = "DONE: " + res.batch.page[0].block[0]._;//лишнее
-        let batches = xmlBuilder.create('projects');       
+        done = 'DONE: ' + res.batch.page[0].block[0]._;//лишнее
+        let batches = xmlBuilder.create('projects');
         if(!err){
           for(let i = 0; i < 75; i++) // число 75 взято от болды
-          //console.log("match: "+ res.batch.page[0].block[i].ATTR.blockName.match(/В\d\d/));
-            batches.ele('answer_type', {"value": res.batch.page[0].block[i]._})
             if(res.batch.page[0].block[i]._ &&
-               !(res.batch.page[0].block[i].ATTR.blockName.match(/В\d\d/) === "null" || res.batch.page[0].block[i].ATTR.blockName.match(/В\d\d/) === null )){
-              done = 'Done: ' + res.batch.page[0].block[i].ATTR.blockName;
-              console.log('blockName: ' + res.batch.page[0].block[i].ATTR.blockName + "; " + res.batch.page[0].block[i].ATTR.blockName.match(/В\d\d/));
-              console.log(i + "Value: " + res.batch.page[0].block[i]._);
-                console.log(i + "l: " + res.batch.page[0].block[i].ATTR.l);
+               !(res.batch.page[0].block[i].ATTR.blockName.match(/В\d\d/) === 'null' || res.batch.page[0].block[i].ATTR.blockName.match(/В\d\d/) === null )){
+                done = 'Done: ' + res.batch.page[0].block[i].ATTR.blockName;
+                //резка изображения
+                /*console.log(i + "l: " + res.batch.page[0].block[i].ATTR.l);
                 console.log(i + "r: " + res.batch.page[0].block[i].ATTR.r);
                 console.log(i + "t: " + res.batch.page[0].block[i].ATTR.t);
-                console.log(i + "b: " + res.batch.page[0].block[i].ATTR.b);
+                console.log(i + "b: " + res.batch.page[0].block[i].ATTR.b);*/
+                console.log('Input Image: ' +(item + '.TIF').match(/TIF|TIFF/));
+                console.log('width: ' + parseInt(res.batch.page[0].block[i].ATTR.r - res.batch.page[0].block[i].ATTR.l));
+                console.log('height: ' + parseInt(res.batch.page[0].block[i].ATTR.b - res.batch.page[0].block[i].ATTR.t));
+                let input_file = fs.readFile(item +'.' + (item + '.TIF').match(/TIF|TIFF/),(err,data)=>{
+                  if(err)
+                    throw(err);
+                  else 
+                    return data;
+                });
+                sharp(fs.readFileSync(item + '.' + (item + '.TIF').match(/TIF|TIFF/)))
+                  .extract(
+                    {'left': parseInt(res.batch.page[0].block[i].ATTR.l), 
+                    'top': parseInt(res.batch.page[0].block[i].ATTR.t),
+                     /*width*/'width' : 1071,//parseInt(res.batch.page[0].block[i].ATTR.r - res.batch.page[0].block[i].ATTR.l),
+                    /*height*/'height': 92,//parseInt(res.batch.page[0].block[i].ATTR.b - res.batch.page[0].block[i].ATTR.t)
+                   })
+                  .toFile(path.join(__dirname,'..','projects/images/') + path.parse(item).name + '_' + i + '.png', (err)=>
+                  {
+                    if(err)
+                      console.log(err);
+                  });
+
+                batches.ele('answer', {
+                  'id' : uuidv1(),
+                  'value' : res.batch.page[0].block[i]._,
+                  'ref' : item + '_' + i + '.png'
+                })
             }
         }
         else{
           console.log(err);
         }
       });
-      
-      let test_xml = "<root>Hello root</root>";
     }
     return done;   
   }
